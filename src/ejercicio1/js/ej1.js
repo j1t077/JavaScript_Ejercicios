c561f4ce2f7c71500fed830f2e175524793a38ce
J1t0 <96486397+j1t077@users.noreply.github.com>
2023-05-28 20:12:23 +0200

    Add files via upload

---
+++ src/ejercicio1/js/ej1.js
@@ -0,0 +1,2 @@
+const hello = document.getElementById("hello");
+hello.innerHTML = "Hello Javascript";
---
+++ src/ejercicio2/js/ex2.js
@@ -0,0 +1,3 @@
+const suma = document.getElementById("suma");
+const resultado = 3 + 5;
+suma.innerHTML = `La suma de 3 + 5 es ${resultado}.`;
